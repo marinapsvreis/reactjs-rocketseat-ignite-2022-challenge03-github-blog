8fa1f69e078611cc2aed90bb70c208c878d44299
Marina Portugal <marinapsvreis@gmail.com>
2023-05-08 19:59:57 -0300

    :lipstick: Configurando arquivos iniciais de estilo

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,12 @@
-export function App() {
+import { ThemeProvider } from 'styled-components'
+import { defaultTheme } from './styles/themes/default'
+import { GlobalStyle } from './styles/global'
 
+export function App() {
   return (
-    <h1>
-      Hello World
-    </h1>
+    <ThemeProvider theme={defaultTheme}>
+      <h1>Hello world</h1>
+      <GlobalStyle />
+    </ThemeProvider>
   )
 }
