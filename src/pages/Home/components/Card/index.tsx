290c527aae668c2d6234003d703c882cc3ce35c3
Marina Portugal <marinapsvreis@gmail.com>
2023-05-25 19:24:51 -0300

    :sparkles: Colocando a data dinamica e traduzindo para pt-br com o moment

--- src/pages/Home/components/Card/index.tsx
+++ src/pages/Home/components/Card/index.tsx
@@ -1,3 +1,4 @@
+import { relativeDateFormatter } from '@/utils/formatter'
 import { Post } from '../..'
 import { CardContainer } from './styles'
 
@@ -7,12 +8,13 @@ interface CardProps {
 
 export function Card({ post }: CardProps) {
   const resumeFromBody = post.body.substring(0, 220)
+  const formattedDate = relativeDateFormatter(post.created_at)
 
   return (
     <CardContainer to={`/post/${post.number}`}>
       <header>
         <h4>{post.title}</h4>
-        <p>Há 1 dia</p>
+        <p>{formattedDate}</p>
       </header>
       <p className="description">{resumeFromBody}...</p>
     </CardContainer>
